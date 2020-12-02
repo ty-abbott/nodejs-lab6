b8edbab646083bb8dfce4d557a046fe6205d9386
Tyler Abbott <tkabbott97@gmail.com>
2020-12-01 19:34:16 -0700

    boolean fix

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -52,7 +52,7 @@ router.get('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
 	try {
 		const task = await Task.findByIdAndUpdate(req.params.id, {$set:{Done:req.body.Done}})
-		console.log(task)
+		console.log("here is the task" + task)
 		if(!task) res.status(404).send('There was no task to update with that ID number')
 		else res.status(200).send(task)
 	}
